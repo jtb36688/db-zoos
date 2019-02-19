15ca27c2e237aeeed171f1d534d7b3fa77cd714d
Jacob Bryan <jtb36688@gmail.com>
2019-02-19 18:15:26 -0500

    POST route handler

--- data/db-model.js
+++ data/db-model.js
@@ -3,5 +3,29 @@ const knexConfig = require('../knexfile.js')
 const db = knex(knexConfig.development);
 
 module.exports = {
-    
+    add,
+    get,
+    getById,
+    delete,
+    modify
+}
+
+function add(table, request) {
+    return db(`${table}`).insert(request)
+}
+
+function get(table) {
+    return db(`${table}`)
+}
+
+function getById(table, id) {
+    return db(`${table}`).where({ id: id })
+}
+
+function delete(table, id) {
+    return db(`${table}`).where({ id: id }).del()
+}
+
+function modify(table, id, request) {
+    return db(`${table}`).where({ id: id }).update(request)
 }
--- index.js
+++ index.js
@@ -13,3 +13,20 @@ const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
+
+server.post('/api/:table', (req, res) => {
+  const { table } = req.params
+  const { name } = req.body
+  const addition = { name }
+  if (!name) {
+    return res.status(400).json({ error: 'Please provide a name for your addition' })
+  }
+  db.add(table)
+  .then(add => {
+    res.status(201).json(add)
+  })
+  .catch(({ code, message }) => {
+    res.status(code).json({ message });
+  });
+})
+
